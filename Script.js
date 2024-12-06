9adf06a7bab984d3b6488fd533836dcc08ef6421
Vineet Singraul <Singraulvineet20@gmail.com>
2024-12-06 23:35:48 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,40 @@
+// Browser Object Maodel
+
+// This Is Show The aleart inside the function 
+// Alart method is Window methode 
+function Aleart() {
+    window.alert("Alart")
+}
+Aleart()
+
+
+
+
+
+// This Is Show The aleart inside the function 
+// Alart method is Window methode 
+function Prompt() {
+
+    // window.prompt("Enter Your age")
+
+   let ans = window.prompt("Enter Your age",20)
+
+   console.log(ans);
+}
+Prompt()
+
+
+
+
+function plase(){
+    let var1 = window.prompt("Enter Num 1 :");
+    let var2 = window.prompt("Enter Num 2 :");
+    let var3 = var1 + var2;
+
+    alert(var3)
+}
+
+plase();
+
+
+
